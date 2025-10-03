1ab8761a657f3c5a19e3690f2c4a8213f29ebc8b
Jesus Fernandez <jesu.fernandez.cortes.al@iespoligonosur.org>
2025-10-03 16:29:37 +0200

    Limpiar archivos innecesarios y mantener solo proyectos Django

--- django-protectora/animales/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Animal, Protectora, Colaborador
-admin.site.register(Animal)
-admin.site.register(Protectora)
-admin.site.register(Colaborador)
